694751bd181dfeb8859a92a72d97a80f7176a165
xXxMuskyWolfxXx <m.klier2003@gmail.com>
2024-07-04 18:16:56 +0200

    added the logs function

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 komenda=$1
+x=1
 case "$komenda" in
+
   "--date") date -u ;;
+
+  "--logs") while [ $x -le 100 ]
+	do
+		touch log"$x".txt
+		echo log"$x".txt >> log"$x".txt
+		echo "utworzony przez skrypt.sh w dniu i godzinie:" >> log"$x".txt
+		date -u >> log"$x".txt
+		x=$[x+1]
+done 
+;;
+
   *) echo "Nic nie wybrałeś"
+
 esac
