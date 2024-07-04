00325331c09294965b29a71d3d52a6ae94aad2a7
xXxMuskyWolfxXx <m.klier2003@gmail.com>
2024-07-04 17:27:19 +0200

    zmiana z if na case

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 komenda=$1
-if [ "$komenda" == "--date" ]
-then
-	date -u
-else
-	echo "brak lub zła komenda"
-fi
+case "$komenda" in
+  "--date") date -u ;;
+  *) echo "Nic nie wybrałeś"
+esac
